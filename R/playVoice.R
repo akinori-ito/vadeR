83570d013a996577e5742d5ecd1a90fb377f789c
Akinori Ito <aito@fw.ipsj.or.jp>
2018-05-20 20:18:21 +0900

    playVoice(): seg parameter added

--- R/playVoice.R
+++ R/playVoice.R
@@ -3,14 +3,17 @@
 #' @importFrom tuneR extractWave play
 #' @export
 #' @param x The input Wave object
+#' @param seg Segmentation obtained by voiceSegment(..,unit="time")
 #' @examples
 #' \dontrun{
 #' library(tuneR)
 #' x <- readWave("speech.wav")
 #' playVoice(x)
 #'}
-playVoice <- function(x) {
-    seg <- voiceSegment(x,unit="time",margin=0.3)
+playVoice <- function(x,seg=NA) {
+    if (is.na(seg)) {
+      seg <- voiceSegment(x,unit="time",margin=0.3)
+    }
     for (i in 1:nrow(seg)) {
         y <- extractWave(x,from=seg$begin[i],to=seg$end[i],xunit="time")
         play(y)
